8951ef144a402ae077ecd79db8d5794da7be77b3
Pavan Koli <kolipavan011@gmail.com>
2023-10-14 14:45:06 +0530

    Added more option slot to header dropdown

--- resources/js/components/PageHeader.vue
+++ resources/js/components/PageHeader.vue
@@ -6,15 +6,16 @@
             </a>
 
             <div class="dropdown">
-                <a class="nav-link" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link" role="button" @click="toggleDropdown" id="defaultDropdown">
                     <div style="width: 32px;height: 32px;" class="bg-primary rounded-circle">
                         <p class="text-uppercase fs-4 text-white text-center fw-bolder">p</p>
                     </div>
                 </a>
 
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
+                <ul class="dropdown-menu dropdown-menu-end" ref="dropdown" aria-labelledby="defaultDropdown">
                     <li><a class="dropdown-item">Pavan Koli</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <slot name="option"></slot>
+                    <li><hr class="dropdown-divider"></li>
                     <li><a class="dropdown-item text-danger" href="#" @click="logout">Logout</a></li>
                 </ul>
             </div>
@@ -23,13 +24,26 @@
 </template>
 
 <script>
+import { Dropdown } from "bootstrap/dist/js/bootstrap.esm.min";
+
 export default {
     name: 'PageHeader',
     props: ['title'],
+    data() {
+        return {
+            dropdown : null,
+        }
+    },
     methods: {
         logout() {
              window.location.href = '/vidmin/logout'
+        },
+        toggleDropdown() {
+            this.dropdown.toggle();
         }
     },
+    mounted() {
+        this.dropdown = new Dropdown(this.$refs.dropdown,{autoClose:'outside'});
+    }
 }
 </script>
--- resources/js/view/PostView.vue
+++ resources/js/view/PostView.vue
@@ -1,7 +1,17 @@
 <template>
     <PageMain>
         <template v-slot:head>
-            <PageHeader title="Post View"></PageHeader>
+            <PageHeader title="Post View">
+                <template v-slot:option>
+                    <router-link 
+                        :to="{ name: 'post-edit',params:{ id: post}}" 
+                        class="dropdown-item"
+                    >
+                        Edit
+                    </router-link>
+                    <li><a class="dropdown-item text-danger" href="#">Delete</a></li>
+                </template>
+            </PageHeader>
         </template>
         <template v-slot:content>
             <!-- content -->
@@ -16,6 +26,11 @@ import ContentView from '../components/ContentView.vue';
 
 export default {
     name: 'PostList',
+    data() {
+        return {
+            post:this.$route.params.id
+        }
+    },
     components: {
         PageMain,
         PageHeader,
