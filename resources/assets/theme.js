9b2081abb775f383f6745c6c23a3598e99426f05
Pavan Koli <kolipavan011@gmail.com>
2023-10-25 16:16:56 +0530

    Updated themes js

--- resources/assets/theme.js
+++ resources/assets/theme.js
@@ -1,1 +1,12 @@
 import 'bootstrap/js/dist/collapse';
+
+(function () { 
+    let videos = document.querySelectorAll('video');
+    videos.forEach(video => (video.onplay = stopPlay));
+
+    function stopPlay(ev) {
+        videos.forEach(video => {
+            if (!video.paused && ev.target !== video) video.pause();
+        })
+    }
+})();
